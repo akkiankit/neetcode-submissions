ad04b438e396eebff2f3cb61124b0611011bfeb8
Ankit Kumar <akkiankitilu@gmail.com>
2026-06-22 20:20:26 +0530

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # return sorted(nums)
+        # Bubble sort
+        # for i in range(len(nums) - 1):
+        #     swapped = False
+        #     for j in range(len(nums) - i - 1):
+        #         if nums[j] > nums[j + 1]:
+        #             nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        #             swapped = True
+        #     if not swapped:
+        #         break
+        # return nums
+        # selection sort
+        for i in range(len(nums)):
+            min_index = i
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            if min_index != i:
+                nums[i], nums[min_index] = nums[min_index], nums[i]
+        return nums
+
+                
+
+         
+
+        
